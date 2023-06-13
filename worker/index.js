6d997376b11d22a9e804db8c00369fe0bf4a7142
lauragift21 <laurandidi21@gmail.com>
2023-06-13 12:20:50 +0200

    update airtable to module worker format

--- worker/index.js
+++ worker/index.js
@@ -1,57 +1,52 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    if (url.pathname === "/submit") {
+      if (request.method !== "POST") {
+        return new Response("Method Not Allowed", {
+          status: 405
+        })
+      }
+    const body = await request.formData();
 
-const createAirtableRecord = body => {
-  return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(AIRTABLE_TABLE_NAME)}`, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-      'Content-type': `application/json`
+    const {
+      first_name,
+      last_name,
+      email,
+      phone,
+      subject,
+      message
+    } = Object.fromEntries(body)
+
+    const reqBody = {
+      fields: {
+        "First Name": first_name,
+        "Last Name": last_name,
+        "Email": email,
+        "Phone Number": phone,
+        "Subject": subject,
+        "Message": message
+      }
     }
-  })
+
+    await createAirtableRecord(env, reqBody)
+    return Response.redirect(env.FORM_URL)
+    }
+  }
 }
 
-const submitHandler = async request => {
-  if (request.method !== "POST") {
-    return new Response("Method Not Allowed", {
-      status: 405
+async function createAirtableRecord(env, body) {
+  try {
+    const result = fetch(`https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${encodeURIComponent(env.AIRTABLE_TABLE_NAME)}`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${env.AIRTABLE_API_TOKEN}`,
+        'Content-Type': 'application/json', 
+      }
     })
+    return result;
+  } catch (error) {
+    console.error(error);
   }
-
-  const body = await request.formData();
-
-  const {
-    first_name,
-    last_name,
-    email,
-    phone,
-    subject,
-    message
-  } = Object.fromEntries(body)
-
-  const reqBody = {
-    fields: {
-      "First Name": first_name,
-      "Last Name": last_name,
-      "Email": email,
-      "Phone Number": phone,
-      "Subject": subject,
-      "Message": message
-    }
-  }
-
-  await createAirtableRecord(reqBody)
-  return Response.redirect(FORM_URL)
-}
-
-async function handleRequest(request) {
-  const url = new URL(request.url)
-
-  if (url.pathname === "/submit") {
-    return submitHandler(request)
-  }
-
-  return new Response.redirect(FORM_URL)
 }
