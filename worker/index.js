7920cdd360e4bb54aa3fd9a66559d33001717448
lauragift21 <laurandidi21@gmail.com>
2023-06-13 12:41:21 +0200

    update airtable tutorial to use latest version of wrangler and module worker format

--- worker/index.js
+++ worker/index.js
@@ -2,38 +2,13 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url)
     if (url.pathname === "/submit") {
-      if (request.method !== "POST") {
-        return new Response("Method Not Allowed", {
-          status: 405
-        })
-      }
-    const body = await request.formData();
-
-    const {
-      first_name,
-      last_name,
-      email,
-      phone,
-      subject,
-      message
-    } = Object.fromEntries(body)
-
-    const reqBody = {
-      fields: {
-        "First Name": first_name,
-        "Last Name": last_name,
-        "Email": email,
-        "Phone Number": phone,
-        "Subject": subject,
-        "Message": message
-      }
+      await submitHandler(request, env)
     }
-
-    await createAirtableRecord(env, reqBody)
     return Response.redirect(env.FORM_URL)
-    }
   }
 }
+
+Ç
 
 async function createAirtableRecord(env, body) {
   try {
